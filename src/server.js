fe19ebe671d726e45933d8890777f04e24c3e69f
Valentin Anikin <anival-94@mail.ru>
2021-11-28 16:17:02 +0300

    feat: add general error handling

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const http = require('http');
 const { getPersons, getPerson, createPerson, updatePerson, deletePerson } = require('./controllers/personController');
-const { validate } = require('uuid'); // ToDo add validation for uuid string
+const { validate } = require('uuid');
 
 let PORT;
 const result = dotenv.config();
@@ -13,43 +13,48 @@ if (result.error) {
 }
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/person' && req.method === 'GET') {
-        getPersons(req, res);
-    } else if (req.url.match(/\/person\/([0-9A-F\-]+)/i) && req.method === 'GET') {
-        const id = req.url.split('/')[2];
-        const isIdValid = validate(id);
+    try {
+        if (req.url === '/person' && req.method === 'GET') {
+            getPersons(req, res);
+        } else if (req.url.match(/\/person\/([0-9A-F\-]+)/i) && req.method === 'GET') {
+            const id = req.url.split('/')[2];
+            const isIdValid = validate(id);
 
-        if (isIdValid) {
-            getPerson(req, res, id);
-        } else {
-            res.writeHead(400, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ message: 'PersonId is not valid' }))
-        }
-    } else if (req.url === '/person' && req.method === 'POST') {
-        createPerson(req, res);
-    } else if (req.url.match(/\/person\/([0-9A-F\-]+)/i) && req.method === 'PUT') {
-        const id = req.url.split('/')[2];
-        const isIdValid = validate(id);
+            if (isIdValid) {
+                getPerson(req, res, id);
+            } else {
+                res.writeHead(400, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ message: 'PersonId is not valid' }))
+            }
+        } else if (req.url === '/person' && req.method === 'POST') {
+            createPerson(req, res);
+        } else if (req.url.match(/\/person\/([0-9A-F\-]+)/i) && req.method === 'PUT') {
+            const id = req.url.split('/')[2];
+            const isIdValid = validate(id);
 
-        if (isIdValid) {
-            updatePerson(req, res, id);
-        } else {
-            res.writeHead(400, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ message: 'PersonId is not valid' }))
-        }
-    } else if (req.url.match(/\/person\/([0-9A-F\-]+)/i) && req.method === 'DELETE') {
-        const id = req.url.split('/')[2];
-        const isIdValid = validate(id);
+            if (isIdValid) {
+                updatePerson(req, res, id);
+            } else {
+                res.writeHead(400, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ message: 'PersonId is not valid' }))
+            }
+        } else if (req.url.match(/\/person\/([0-9A-F\-]+)/i) && req.method === 'DELETE') {
+            const id = req.url.split('/')[2];
+            const isIdValid = validate(id);
 
-        if (isIdValid) {
-            deletePerson(req, res, id);
+            if (isIdValid) {
+                deletePerson(req, res, id);
+            } else {
+                res.writeHead(400, { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ message: 'PersonId is not valid' }))
+            }
         } else {
-            res.writeHead(400, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify({ message: 'PersonId is not valid' }))
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Route not found' }))
         }
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: 'Route not found' }))
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: error.message }))
     }
 })
 
